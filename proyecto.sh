97b8c38998edf6a55f03b3139cb54910ebb96cd1
vboxuser <vboxuser@Debian1.myguest.virtualbox.org>
2026-05-20 09:33:10 -0600

    Estructura inicial del programa principal

---
+++ proyecto.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Modulo:
+#Objetivo:
+#Version: 1
+#Autor:
+#	Duron Mendoza Antonio
+#	Lopez Acevedo Tania Jaquelin
+#	Raygoza Castaleda Ulises
+#	Vazquez Delgado Cristian de Jesus
+#Modo de uso:
+
+menu(){
+
+}
+
+#codigo main
+	opcion=0
+	while["$opcion" != "0"]
+	do
+		menu
+	done
